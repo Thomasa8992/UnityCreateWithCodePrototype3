960382de03019b4cab69f966acd14c2a01b93a5e
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-27 00:31:49 -0500

    Added code to make the player jump using force and gravity, also added code to keep the player from double jumping. Added an obstacle prefab and a script to make the obstacle and background move left

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRigidBody;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool playerIsOnTheGround = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) && playerIsOnTheGround) {
+            playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerIsOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        playerIsOnTheGround = true;
     }
 }
