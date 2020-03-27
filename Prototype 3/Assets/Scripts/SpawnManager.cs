7a443589fb10be93ecf4a16aedf952917caa8287
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-27 01:07:56 -0500

    Added logic to spawn multiple obstacles at certain intervals

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    public Vector3 obstacleSpawnPosition = new Vector3(20, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnObstacle() {
+        Instantiate(obstaclePrefab, obstacleSpawnPosition, obstaclePrefab.transform.rotation);
+    }
+}
