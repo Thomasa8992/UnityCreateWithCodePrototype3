7947c59e3af7809b3a9738d0e74def84ab60f8e0
Adrian Thomas <thomasa8992@hotmail.com>
2020-03-29 00:32:25 -0500

    Added logic to trigger a death animation on collision with an obstacle

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -38,6 +38,8 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle")) {
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
         }
     }
 }
